672e222113149d3cd4a9ad45e51c409c4556188a
ovanderzee <hastalavista@dds.nl>
2020-09-04 09:57:04 +0200

    edge case tests for intersection/combnination

--- src/array/arrayCombination.test.js
+++ src/array/arrayCombination.test.js
@@ -6,3 +6,21 @@ test('arrayCombination returns union of two arrays', () => {
     let output = arrayCombination(array1, array2)
     expect(output).toEqual([1, 2, 3, 4])
 })
+
+test('arrayCombination throws when the first array is bad', () => {
+    let array1 = null
+    let array2 = [5, 6, 6]
+    let action = function () {
+        arrayCombination(array1, array2)
+    }
+    expect(action).toThrow()
+})
+
+test('v throws when the second array is bad', () => {
+    let array1 = [1, 2, 3, 1, 2, 3]
+    let array2 = null
+    let action = function () {
+        arrayCombination(array1, array2)
+    }
+    expect(action).toThrow()
+})
--- src/array/arrayIntersection.test.js
+++ src/array/arrayIntersection.test.js
@@ -6,3 +6,28 @@ test('arrayIntersection returns mutual values of two arrays', () => {
     let output = arrayIntersection(array1, array2)
     expect(output).toEqual([2, 3])
 })
+
+test('arrayIntersection returns absent mutual values of two arrays', () => {
+    let array1 = [1, 2, 3, 1, 2, 3]
+    let array2 = [5, 6, 6]
+    let output = arrayIntersection(array1, array2)
+    expect(output).toEqual([])
+})
+
+test('arrayIntersection throws when the first array is bad', () => {
+    let array1 = null
+    let array2 = [5, 6, 6]
+    let action = function () {
+        arrayIntersection(array1, array2)
+    }
+    expect(action).toThrow()
+})
+
+test('arrayIntersection throws when the second array is bad', () => {
+    let array1 = [1, 2, 3, 1, 2, 3]
+    let array2 = null
+    let action = function () {
+        arrayIntersection(array1, array2)
+    }
+    expect(action).toThrow()
+})
